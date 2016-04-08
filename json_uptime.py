21dcc5070425804910f97f1f006f0b209fac53ae
baigm <baigm@users.noreply.github.com>
2016-04-07 21:02:54 -0400

    Parse and return more data from 'uptime' command

--- json_uptime.py
+++ json_uptime.py
@@ -5,16 +5,25 @@ import subprocess
 import argparse
 
 def get_uptime():
-    uptime = ''
+    info = {}
 
     try:
         output = subprocess.check_output(["uptime"])
     except subprocess.CalledProcessError as e:
         print (e.output)
     else:
-        uptime = output.split()[2][0:-1]
+        output_list = output.split(',',3)
+        info['timestamp'] = output_list[0].split()[0]
+        days_running = output_list[0].split()[2]
+        hours_running = output_list[1].strip()
+        info['uptime'] = days_running + ' days, and ' + hours_running + ' hours'
+        info['current_users'] = output_list[2].split()[0]
+        load_average = output_list[3].split()
+        info['load_average_1min'] = load_average[2].strip(',')
+        info['load_average_5min'] = load_average[3].strip(',')
+        info['load_average_15min'] = load_average[4]
 
-    return {'uptime': uptime}
+    return info
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
